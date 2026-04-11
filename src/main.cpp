fb09b73c6fc83c380738beed5ea9093200ba25b6
Michał <284094@student.pwr.edu.pl>
2026-04-11 17:44:40 +0200

    Dodana pełna implementacja Tablicy Dynamicznej

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "DataGenerator.hpp"
+#include "DataGenerator.h"
 int main() {
     DataGenerator::generateRandomData(5000, 12345, "data/data_5000.txt");
     DataGenerator::generateRandomData(10000, 12345, "data/data_10000.txt");
