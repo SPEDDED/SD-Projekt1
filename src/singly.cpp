da563f829a0c0c93cfba406434a11b049b51467b
284143-szef <284143@student.pwr.edu.pl>
2026-04-19 21:10:44 +0200

    Update and rename SinglyLinkedList.cpp to singly.cpp

--- src/singly.cpp
+++ src/singly.cpp
@@ -1,24 +1,23 @@
-#include "SinglyLinkedList.h"
+#include "singly.h"
 #include <iostream>
 
 using namespace std;
 
-// Inicjalizacja pustej listy
-SinglyLinkedList::SinglyLinkedList() : head(nullptr), tail(nullptr), size(0) {}
+singly::singly() : head(nullptr), tail(nullptr), size(0) {}
 
-// Destruktor usuwa wszystkie elementy, aby zwolnić pamięć
-SinglyLinkedList::~SinglyLinkedList() {
-    while (head) usunPoczatek();
+// Destruktor usuwa wszystkie elementy
+singly::~singly() {
+    while (head) usun_poczatek();
 }
 
-void SinglyLinkedList::dodajPoczatek(int wartosc) {
+void singly::dod_poczatek(int wartosc) {
     Node* newNode = new Node{wartosc, head}; // Nowy węzeł wskazuje na obecny head
     head = newNode;                          // Nowy węzeł staje się głową
-    if (!tail) tail = head;                  // Jeśli lista była pusta, head jest też ogonem
+    if (!tail) tail = head;                  // Jeśli lista była pusta, head = tail
     size++;
 }
 
-void SinglyLinkedList::dodajKoniec(int wartosc) {
+void singly::dod_koniec(int wartosc) {
     Node* newNode = new Node{wartosc, nullptr};
     if (tail) {
         tail->next = newNode; // Obecny ogon wskazuje na nowy element
@@ -29,9 +28,9 @@ void SinglyLinkedList::dodajKoniec(int wartosc) {
     size++;
 }
 
-void SinglyLinkedList::dodajNaMiejsce(int wartosc, int indeks) {
-    if (indeks <= 0) { dodajPoczatek(wartosc); return; }
-    if (indeks >= size) { dodajKoniec(wartosc); return; }
+void singly::dod_index(int wartosc, int indeks) {
+    if (indeks <= 0) { dod_poczatek(wartosc); return; }
+    if (indeks >= size) { dod_koniec(wartosc); return; }
 
     Node* temp = head;
     // Przejście do elementu poprzedzającego miejsce wstawienia
@@ -42,7 +41,13 @@ void SinglyLinkedList::dodajNaMiejsce(int wartosc, int indeks) {
     size++;
 }
 
-void SinglyLinkedList::usunPoczatek() {
+
+
+
+
+
+
+void singly::usun_poczatek() {
     if (!head) return;
     Node* temp = head;     // Zapamiętanie elementu do usunięcia
     head = head->next;     // Przesunięcie głowy na następny element
@@ -51,8 +56,8 @@ void SinglyLinkedList::usunPoczatek() {
     if (size == 0) tail = nullptr; // Jeśli lista stała się pusta
 }
 
-void SinglyLinkedList::usunKoniec() {
-    if (size <= 1) { usunPoczatek(); return; }
+void singly::usun_koniec() {
+    if (size <= 1) { usun_poczatek(); return; }
     Node* temp = head;
     // Szukanie przedostatniego elementu
     while (temp->next != tail) temp = temp->next;
@@ -63,21 +68,27 @@ void SinglyLinkedList::usunKoniec() {
     size--;
 }
 
-void SinglyLinkedList::usunElement(int indeks) {
-    if (indeks <= 0) { usunPoczatek(); return; }
-    if (indeks >= size - 1) { usunKoniec(); return; }
+void singly::usun_element(int indeks) {
+    if (indeks <= 0) { usun_poczatek(); return; }
+    if (indeks >= size - 1) { usun_koniec(); return; }
 
     Node* temp = head;
     // Przejście do elementu przed tym, który chcemy usunąć
     for (int i = 0; i < indeks - 1; i++) temp = temp->next;
     
-    Node* doUsuniecia = temp->next;
-    temp->next = doUsuniecia->next; // Ominięcie usuwanego węzła w łańcuchu
-    delete doUsuniecia;             // Zwolnienie pamięci
+    Node* lota = temp->next;
+    temp->next = lota->next; // Ominięcie usuwanego węzła w łańcuchu
+    delete lota;             // Zwolnienie pamięci
     size--;
 }
 
-int SinglyLinkedList::znajdz(int wartosc) {
+
+
+
+
+
+
+int singly::znajdz(int wartosc) {
     Node* temp = head;
     int idx = 0;
     while (temp) { // Iteracja po wszystkich węzłach
@@ -88,7 +99,7 @@ int SinglyLinkedList::znajdz(int wartosc) {
     return -1; // Nie znaleziono
 }
 
-void SinglyLinkedList::wyswietl() {
+void singly::wyswietl() {
     Node* temp = head;
     while (temp) {
         cout << temp->data << " ";
