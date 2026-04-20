93b119faef8af17400749e921091c21e8af62b50
Michał <284094@student.pwr.edu.pl>
2026-04-20 18:04:59 +0200

    Modyfikacja wyszukiwania, wyszukiwanie z palca dla każdej próby zostało przerobione na pętlę

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,9 @@
 using namespace std;
 using namespace std::chrono;
 
+
 void Czas(nanoseconds duration) {
-    cout << ">>> Sredni czas: " << duration.count() << " ns" << endl;
+    cout << ">>> Sredni czas z 100 prob: " << duration.count() << " ns" << endl;
 }
 
 int main() {
@@ -34,39 +35,42 @@ int main() {
         cout << "9. Wczytaj z istniejacego juz pliku" << endl;
         cout << "0. Wyjscie" << endl;
         cout << "Wybor: ";
-        int wielkosc;
-        cin >> wielkosc;
+        int wyborWielkosci;
+        cin >> wyborWielkosci;
 
-        if (wielkosc == 0) break;
+        if (wyborWielkosci == 0) break;
 
         string nazwaPliku = "";
 
-        if (wielkosc >= 1 && wielkosc <= 8) {
+
+        if (wyborWielkosci >= 1 && wyborWielkosci <= 8) {
             int n = 0;
-            if (wielkosc == 1) n = 5000;
-            else if (wielkosc == 2) n = 10000;
-            else if (wielkosc == 3) n = 20000;
-            else if (wielkosc == 4) n = 40000;
-            else if (wielkosc == 5) n = 60000;
-            else if (wielkosc == 6) n = 80000;
-            else if (wielkosc == 7) n = 100000;
-            else if (wielkosc == 8) n = 120000;
+            if (wyborWielkosci == 1) n = 5000;
+            else if (wyborWielkosci == 2) n = 10000;
+            else if (wyborWielkosci == 3) n = 20000;
+            else if (wyborWielkosci == 4) n = 40000;
+            else if (wyborWielkosci == 5) n = 60000;
+            else if (wyborWielkosci == 6) n = 80000;
+            else if (wyborWielkosci == 7) n = 100000;
+            else if (wyborWielkosci == 8) n = 120000;
 
             nazwaPliku = "data_" + to_string(n) + ".txt";
+
             DataGenerator::generateRandomData(n, 12345, nazwaPliku);
         } 
-        else if (wielkosc == 9) {
+        else if (wyborWielkosci == 9) {
             cout << "Podaj nazwe pliku: ";
             cin >> nazwaPliku;
         }
 
-        if (wielkosc >= 1 && wielkosc <= 9) {
+        if (wyborWielkosci >= 1 && wyborWielkosci <= 9) {
             cout << "\nWYBIERZ STRUKTURE DO TESTOW:" << endl;
             cout << "1. Tablica dynamiczna" << endl;
             cout << "2. Lista jednokierunkowa" << endl;
             cout << "3. Lista dwukierunkowa" << endl;
             cout << "Wybor: ";
             cin >> struktura;
+
 
             d = DynamicArray(); 
             s = singly(); 
@@ -91,9 +95,9 @@ int main() {
             }
         }
 
-        int menu;
+        int menuOperacji;
         do {
-            cout << "\n--- OPERACJE NA STRUKTURZE ---" << endl;
+            cout << "\n--- OPERACJE NA STRUKTURZE (POMIAR SREDNI) ---" << endl;
             cout << "1. Dodaj na poczatek" << endl;
             cout << "2. Dodaj na koniec" << endl;
             cout << "3. Dodaj na miejsce" << endl;
@@ -104,121 +108,125 @@ int main() {
             cout << "8. Wyswietl zawartosc" << endl;
             cout << "0. Powrot do menu glownego" << endl;
             cout << "Wybor: ";
-            cin >> menu;
+            cin >> menuOperacji;
 
-            if (menu == 0) break;
+            if (menuOperacji == 0) break;
 
             int val = 0, idx = 0;
-            if (menu >= 1 && menu <= 3) { cout << "Podaj wartosc: "; cin >> val; }
-            if (menu == 3 || menu == 6) { cout << "Podaj indeks: "; cin >> idx; }
-            if (menu == 7) { cout << "Podaj szukana wartosc: "; cin >> val; }
+            if (menuOperacji >= 1 && menuOperacji <= 3) { cout << "Podaj wartosc: "; cin >> val; }
+            if (menuOperacji == 3 || menuOperacji == 6) { cout << "Podaj indeks: "; cin >> idx; }
+            if (menuOperacji == 7) { cout << "Podaj szukana wartosc: "; cin >> val; }
 
-            if (menu >= 1 && menu <= 7) {
-    long long suma = 0;
-    const int proby = 100;
+            if (menuOperacji >= 1 && menuOperacji <= 7) {
+                long long sumaNanosekund = 0;
+                const int proby = 100; 
 
-    for (int i = 0; i < proby; i++) {
-        high_resolution_clock::time_point t1, t2;
+                for (int i = 0; i < proby; i++) {
+                    high_resolution_clock::time_point t1, t2;
 
-        if (struktura == 1) {
-            if (menu == 1) { 
-                t1 = high_resolution_clock::now(); d.dod_poczatek(val); t2 = high_resolution_clock::now();
-                d.usun_poczatek(); 
-            }
-            else if (menu == 2) { 
-                t1 = high_resolution_clock::now(); d.dod_koniec(val); t2 = high_resolution_clock::now();
-                d.usun_koniec(); 
-            }
-            else if (menu == 3) { 
-                t1 = high_resolution_clock::now(); d.dod_index(val, idx); t2 = high_resolution_clock::now();
-                d.usun_element(idx); 
-            }
-            else if (menu == 4) { 
-                t1 = high_resolution_clock::now(); d.usun_poczatek(); t2 = high_resolution_clock::now();
-                d.dod_poczatek(val); 
-            }
-            else if (menu == 5) { 
-                t1 = high_resolution_clock::now(); d.usun_koniec(); t2 = high_resolution_clock::now();
-                d.dod_koniec(val); 
-            }
-            else if (menu == 6) { 
-                t1 = high_resolution_clock::now(); d.usun_element(idx); t2 = high_resolution_clock::now();
-                d.dod_index(val, idx); 
-            }
-            else if (menu == 7) {
-                t1 = high_resolution_clock::now(); d.znajdz(val); t2 = high_resolution_clock::now();
-            }
-        } 
-        else if (struktura == 2) { 
-            if (menu == 1) { 
-                t1 = high_resolution_clock::now(); s.dod_poczatek(val); t2 = high_resolution_clock::now();
-                s.usun_poczatek(); 
-            }
-            else if (menu == 2) { 
-                t1 = high_resolution_clock::now(); s.dod_koniec(val); t2 = high_resolution_clock::now();
-                s.usun_koniec(); 
-            }
-            else if (menu == 3) { 
-                t1 = high_resolution_clock::now(); s.dod_index(val, idx); t2 = high_resolution_clock::now();
-                s.usun_element(idx); 
-            }
-            else if (menu == 4) { 
-                t1 = high_resolution_clock::now(); s.usun_poczatek(); t2 = high_resolution_clock::now();
-                s.dod_poczatek(val); 
-            }
-            else if (menu == 5) { 
-                t1 = high_resolution_clock::now(); s.usun_koniec(); t2 = high_resolution_clock::now();
-                s.dod_koniec(val); 
-            }
-            else if (menu == 6) { 
-                t1 = high_resolution_clock::now(); s.usun_element(idx); t2 = high_resolution_clock::now();
-                s.dod_index(val, idx); 
-            }
-            else if (menu == 7) {
-                t1 = high_resolution_clock::now(); s.znajdz(val); t2 = high_resolution_clock::now();
-            }
-        }
-        else if (struktura == 3) {
-            if (menu == 1) { 
-                t1 = high_resolution_clock::now(); dl.dod_poczatek(val); t2 = high_resolution_clock::now();
-                dl.usun_poczatek(); 
-            }
-            else if (menu == 2) { 
-                t1 = high_resolution_clock::now(); dl.dod_koniec(val); t2 = high_resolution_clock::now();
-                dl.usun_koniec(); 
-            }
-            else if (menu == 3) { 
-                t1 = high_resolution_clock::now(); dl.dod_index(val, idx); t2 = high_resolution_clock::now();
-                dl.usun_element(idx); 
-            }
-            else if (menu == 4) { 
-                t1 = high_resolution_clock::now(); dl.usun_poczatek(); t2 = high_resolution_clock::now();
-                dl.dod_poczatek(val); 
-            }
-            else if (menu == 5) { 
-                t1 = high_resolution_clock::now(); dl.usun_koniec(); t2 = high_resolution_clock::now();
-                dl.dod_koniec(val); 
-            }
-            else if (menu == 6) { 
-                t1 = high_resolution_clock::now(); dl.usun_element(idx); t2 = high_resolution_clock::now();
-                dl.dod_index(val, idx); 
-            }
-            else if (menu == 7) {
-                t1 = high_resolution_clock::now(); dl.znajdz(val); t2 = high_resolution_clock::now();
-            }
-        }
 
-        suma += duration_cast<nanoseconds>(t2 - t1).count();
-    }
-    Czas(nanoseconds(suma / proby));
-} 
-            else if (menu == 8) {
+                    if (struktura == 1) {
+                        if (menuOperacji == 1) { 
+                            t1 = high_resolution_clock::now(); d.dod_poczatek(val); t2 = high_resolution_clock::now();
+                            d.usun_poczatek(); 
+                        }
+                        else if (menuOperacji == 2) { 
+                            t1 = high_resolution_clock::now(); d.dod_koniec(val); t2 = high_resolution_clock::now();
+                            d.usun_koniec(); 
+                        }
+                        else if (menuOperacji == 3) { 
+                            t1 = high_resolution_clock::now(); d.dod_index(val, idx); t2 = high_resolution_clock::now();
+                            d.usun_element(idx); 
+                        }
+                        else if (menuOperacji == 4) { 
+                            t1 = high_resolution_clock::now(); d.usun_poczatek(); t2 = high_resolution_clock::now();
+                            d.dod_poczatek(val); 
+                        }
+                        else if (menuOperacji == 5) { 
+                            t1 = high_resolution_clock::now(); d.usun_koniec(); t2 = high_resolution_clock::now();
+                            d.dod_koniec(val); 
+                        }
+                        else if (menuOperacji == 6) { 
+                            t1 = high_resolution_clock::now(); d.usun_element(idx); t2 = high_resolution_clock::now();
+                            d.dod_index(val, idx); 
+                        }
+                        else if (menuOperacji == 7) {
+                            t1 = high_resolution_clock::now(); d.znajdz(val); t2 = high_resolution_clock::now();
+                        }
+                    } 
+
+                    else if (struktura == 2) { 
+                        if (menuOperacji == 1) { 
+                            t1 = high_resolution_clock::now(); s.dod_poczatek(val); t2 = high_resolution_clock::now();
+                            s.usun_poczatek(); 
+                        }
+                        else if (menuOperacji == 2) { 
+                            t1 = high_resolution_clock::now(); s.dod_koniec(val); t2 = high_resolution_clock::now();
+                            s.usun_koniec(); 
+                        }
+                        else if (menuOperacji == 3) { 
+                            t1 = high_resolution_clock::now(); s.dod_index(val, idx); t2 = high_resolution_clock::now();
+                            s.usun_element(idx); 
+                        }
+                        else if (menuOperacji == 4) { 
+                            t1 = high_resolution_clock::now(); s.usun_poczatek(); t2 = high_resolution_clock::now();
+                            s.dod_poczatek(val); 
+                        }
+                        else if (menuOperacji == 5) { 
+                            t1 = high_resolution_clock::now(); s.usun_koniec(); t2 = high_resolution_clock::now();
+                            s.dod_koniec(val); 
+                        }
+                        else if (menuOperacji == 6) { 
+                            t1 = high_resolution_clock::now(); s.usun_element(idx); t2 = high_resolution_clock::now();
+                            s.dod_index(val, idx); 
+                        }
+                        else if (menuOperacji == 7) {
+                            t1 = high_resolution_clock::now(); s.znajdz(val); t2 = high_resolution_clock::now();
+                        }
+                    }
+
+                    else if (struktura == 3) {
+                        if (menuOperacji == 1) { 
+                            t1 = high_resolution_clock::now(); dl.dod_poczatek(val); t2 = high_resolution_clock::now();
+                            dl.usun_poczatek(); 
+                        }
+                        else if (menuOperacji == 2) { 
+                            t1 = high_resolution_clock::now(); dl.dod_koniec(val); t2 = high_resolution_clock::now();
+                            dl.usun_koniec(); 
+                        }
+                        else if (menuOperacji == 3) { 
+                            t1 = high_resolution_clock::now(); dl.dod_index(val, idx); t2 = high_resolution_clock::now();
+                            dl.usun_element(idx); 
+                        }
+                        else if (menuOperacji == 4) { 
+                            t1 = high_resolution_clock::now(); dl.usun_poczatek(); t2 = high_resolution_clock::now();
+                            dl.dod_poczatek(val); 
+                        }
+                        else if (menuOperacji == 5) { 
+                            t1 = high_resolution_clock::now(); dl.usun_koniec(); t2 = high_resolution_clock::now();
+                            dl.dod_koniec(val); 
+                        }
+                        else if (menuOperacji == 6) { 
+                            t1 = high_resolution_clock::now(); dl.usun_element(idx); t2 = high_resolution_clock::now();
+                            dl.dod_index(val, idx); 
+                        }
+                        else if (menuOperacji == 7) {
+                            t1 = high_resolution_clock::now(); dl.znajdz(val); t2 = high_resolution_clock::now();
+                        }
+                    }
+
+                    sumaNanosekund += duration_cast<nanoseconds>(t2 - t1).count();
+                }
+
+                Czas(nanoseconds(sumaNanosekund / proby));
+            } 
+            else if (menuOperacji == 8) {
                 if (struktura == 1) d.wyswietl();
                 else if (struktura == 2) s.wyswietl();
                 else if (struktura == 3) dl.wyswietl();
             }
 
-        } while (menu != 0);
+        } while (menuOperacji != 0);
 
     } while (true);
 
